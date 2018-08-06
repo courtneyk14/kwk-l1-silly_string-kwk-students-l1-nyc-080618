2e506f6d03d65edb7201ded51ed61a7c713b0e12
courtneyk14 <cbrookelley14@gmail.com>
2018-08-06 18:51:02 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -36,7 +36,7 @@ puts "There are now #{new_letter_count} in #{bigger_word}"
 
 # Challenge 6: Capitalize this sentence properly
 
-proper_sentence = "i really like programming.".upcase # Put a . after the string to capitalize it properly.
+proper_sentence = "i really like programming.".upcase
 
 puts proper_sentence
 
